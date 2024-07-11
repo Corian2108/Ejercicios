5bc7c3b6f414096a9967cab845e8618257fa5f5d
Corian2108 <jefferson.ruiz2108@gmail.com>
2024-07-11 16:37:27 -0500

    update/muestra resultados en tabla

--- Javascript/solver.js
+++ Javascript/solver.js
@@ -1,6 +1,8 @@
 var mult1 = 3
 var mult2 = 5
 var multiples = []
+var multiples3 = []
+var multiples5 = []
 
 function Counter(num, limit) {
     if (num < limit) {
@@ -14,18 +16,46 @@ function Counter(num, limit) {
 
 function IsMultiple(num) {
     if (num % mult1 == 0) {
+        multiples3.push(num)
+        if (num % mult2 == 0) {
+            multiples5.push(num)
+        }
         return true
     } else if (num % mult2 == 0) {
+        multiples5.push(num)
         return true
     } else {
         return false
     }
 }
 
-// function Sum(total, value) {
-//     return total + value
-// }
-Counter(1,10)
+function ShowResult() {
+    multiples = []
+    multiples3 = []
+    multiples5 = []
+    Counter(1, 100)
 
-console.log(multiples)
-// console.log(multiples.reduce(Sum()))
+    let table = document.getElementById("tabla")
+    table.innerHTML = ""
+
+    let td1 = "<tr><td>"
+    let td2 = "</td></tr>"
+
+    multiples.forEach((item, index) => {
+        let tr = table.appendChild(document.createElement("tr"))
+        let mult3 = ''
+        let mult5 = ''
+
+        if (multiples3[index] !== undefined) {
+            mult3 = multiples3[index]
+        }
+        if (multiples5[index] !== undefined) {
+            mult5 = multiples5[index]
+        }
+        let line = td1 + (index + 1) + td2
+            + td1 + item + td2
+            + td1 + mult3 + td2
+            + td1 + mult5 + td2
+        tr.innerHTML = line
+    })
+}
